21416a3e53c4d3ea55b2bcd214dbb1a136843662
Baris Onat <barisonat7@gmail.com>
2022-07-09 18:06:00 -0400

    changed default endpoint

--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -32,7 +32,7 @@ export default function AppContext({children}){
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const endpt = null; // change this to go to specific PORT
+    const endpt = "http://127.0.0.1:5000/"; // change this to go to specific PORT
     const newSocket = endpt ? socketIOClient(endpt) : socketIOClient();
     setSocket(newSocket);
     return () => newSocket.close();
