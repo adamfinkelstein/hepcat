26ccc462bf583fe71f4ae1819cd4887363575428
Adam Finkelstein <af@princeton.edu>
2022-07-14 22:41:08 -0400

    overall format and button style on upload page

--- app/upload/forms.py
+++ app/upload/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, FileField
-from wtforms.validators import DataRequired
+# from wtforms.validators import DataRequired
 
 class UploadForm(FlaskForm):
     file = FileField('Your CSV File')
-    submit = SubmitField('Submit')
+    submit = SubmitField('Upload CSV')
 
