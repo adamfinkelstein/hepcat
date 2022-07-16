0bf0a568cd97784d4e2868ca8f4adc9590f16608
Adam Finkelstein <af@princeton.edu>
2022-07-16 18:39:58 -0400

    about page contents now read from markdown file in public folder

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,10 +1,19 @@
+import { useState, useEffect } from 'react'
 import Container from 'react-bootstrap/Container'
+import ReactMarkdown from 'react-markdown'
 
 export default function AboutPage() {
+
+    const aboutFile = 'about.md'
+    const [aboutText, setAboutText] = useState('')
+
+    useEffect(() => {
+        fetch(aboutFile).then(res => res.text()).then(text => setAboutText(text))
+    },[])
+
     return (
-        <Container className='preferences-container'>
-            <h1>About</h1>
-            <p>This page is about this app.</p>
+        <Container className='about-container'>
+            <ReactMarkdown children={aboutText} />
         </Container>
     );
 }
