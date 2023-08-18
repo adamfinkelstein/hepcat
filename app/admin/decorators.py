bee6efb0ec843acc2466cb7e852bc86569cf37ba
Adam Finkelstein <af@princeton.edu>
2023-08-18 12:14:53 -0400

    admin_required_for_route aborts with 404 on failure

--- app/admin/decorators.py
+++ app/admin/decorators.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from flask import redirect, url_for
+from flask import abort
 from app.util import current_user_is_admin, current_user_is_super
 
 
@@ -7,7 +7,7 @@ def admin_required_for_route(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         if not current_user_is_admin():
-            return redirect(url_for('auth.login'))
+            return abort(404)
         return f(*args, **kwargs)
 
     return decorated
@@ -17,7 +17,7 @@ def super_required_for_route(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         if not current_user_is_super():
-            return redirect(url_for('auth.login'))
+            return abort(404)
         return f(*args, **kwargs)
 
     return decorated
