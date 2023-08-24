7b37fddf176c1b231d6ad7ca57f8e353f1391e64
Miguel Grinberg <miguel.grinberg@gmail.com>
2023-08-24 12:11:13 +0100

    Remove unused form support in backend

--- app/main/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-
-# AF: Is this even used?
-class NameForm(FlaskForm):
-    name = StringField("What is your name?", validators=[DataRequired()])
-    submit = SubmitField("Submit")
