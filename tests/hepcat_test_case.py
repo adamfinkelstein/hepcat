5284a3d98314f5d9cc14b647f40dcce69a1e6ec5
Adam Finkelstein <af@princeton.edu>
2025-06-08 08:52:08 -0400

    revert to old testing

--- tests/hepcat_test_case.py
+++ tests/hepcat_test_case.py
@@ -15,7 +15,7 @@ class HepcatTestCase(unittest.TestCase):
 
         # create a test database
         db.create_all()  # is this needed? not harmful.
-        # the following could probably be replaced with:
+        # XXX the following could probably be replaced with:
         # read_test_csv_files()
         read_csv("tests/test-data/users.csv")
         read_csv("tests/test-data/papers.csv")
