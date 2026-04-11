53c5a4ba70fc0d63a4ae7dd8ef3b6bcfc2f124d5
Adam Finkelstein <af@princeton.edu>
2026-04-11 09:24:43 -0400

    add deploy scripts

--- deploy-scripts/deploy-code-to-backup.sh
+++ deploy-scripts/deploy-code-to-backup.sh
@@ -8,9 +8,12 @@ ssh ubuntu@backup.hepcat.app supervisorctl stop hepcat
 cd /home/ubuntu/hepcat/deploy-scripts
 source deploy-code-only.sh
 
-# Copy this entire directory (including code) to backup server
+# Copy this entire directory (including code) to backup server.
+# -a flag means "archive" mode which preserves:
+#    * symbolic links, important in venv, as well as
+#    * permissions, timestamp, owner, group
 cd /home/ubuntu
-rsync -rvz --delete hepcat/ ubuntu@backup.hepcat.app:hepcat/
+rsync -avz --delete hepcat/ ubuntu@backup.hepcat.app:hepcat/
 
 # Start hepcat locally and at backup server
 sudo supervisorctl start hepcat
