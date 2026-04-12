1b703bd41b520d30cc4a78e4d97622cd6f37f4ea
Adam Finkelstein <af@princeton.edu>
2026-04-12 11:56:55 -0400

    ID backup server

--- src/contexts/AdminContext.jsx
+++ src/contexts/AdminContext.jsx
@@ -192,6 +192,7 @@ export default function AdminContext({ children }) {
         roomBackups,
         backupFiles,
         requestBackupList,
+        isBackupServer,
       }}
     >
       {children}
