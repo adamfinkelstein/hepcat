eab3e6a68e2cc6299b2dc274e8b702bafa24c4ad
Adam Finkelstein <af@princeton.edu>
2026-04-12 11:24:27 -0400

    fix .env at backup

--- deploy-scripts/deploy-code-to-backup.sh
+++ deploy-scripts/deploy-code-to-backup.sh
@@ -8,12 +8,28 @@ ssh ubuntu@backup.hepcat.app sudo supervisorctl stop hepcat
 cd /home/ubuntu/hepcat/deploy-scripts
 source deploy-code-only.sh
 
+# Modify .env for backup server deployment
+TMP_DIR="/home/ubuntu/hepcat/tmp"
+ENV_FILE="/home/ubuntu/hepcat/.env"
+ENV_TMP="$TMP_DIR/.env.original"
+
+# Back up the original .env
+mkdir -p $TMP_DIR
+mv $ENV_FILE $ENV_TMP
+
+# Write modified .env (with backup server flag)
+cp $ENV_TMP $ENV_FILE
+printf "\nDB_BACKUP_SERVER=true\n" >> $ENV_FILE
+
 # Copy this entire directory (including code) to backup server.
 # -a flag means "archive" mode which preserves:
 #    * symbolic links, important in venv, as well as
 #    * permissions, timestamp, owner, group
 cd /home/ubuntu
 rsync -avz --delete hepcat/ ubuntu@backup.hepcat.app:hepcat/
+
+# Restore the original .env
+mv $ENV_TMP $ENV_FILE
 
 # Start hepcat locally and at backup server
 sudo supervisorctl start hepcat
