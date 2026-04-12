4e25be07f05a097f4f3740088dcf44814f6030e0
Adam Finkelstein <af@princeton.edu>
2026-04-11 22:46:47 -0400

    backup now btn

--- src/pages/UploadsPage.jsx
+++ src/pages/UploadsPage.jsx
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 Adam Finkelstein
+// Copyright (c) 2025-2026 Adam Finkelstein
 // Licensed under the Apache 2.0 License. See LICENSE file for details.
 
 import Container from 'react-bootstrap/Container';
@@ -29,6 +29,10 @@ export default function UploadsPage() {
   const pendingTitle = pendingList.length
     ? 'Pending Files:'
     : 'No files pending.';
+
+  const handleBackupNowBtn = () => {
+    socketEmit('admin_backup_now');
+  };
 
   const handleSendBtn = (event) => {
     event.preventDefault();
@@ -114,6 +118,12 @@ export default function UploadsPage() {
             <h2>Extra Admin Functions</h2>
           </div>
           <Stack className="mt-4 mb-5" direction="vertical" gap={4}>
+            <Stack direction="horizontal">
+              <Button className="btn-warning" onClick={handleBackupNowBtn}>
+                Backup Now
+              </Button>
+              &nbsp;&nbsp;Save a backup of the database right now.
+            </Stack>
             <Stack direction="horizontal">
               <Button
                 className="btn-warning"
